b70a445408e94bb79c103d7845f367f62c86eb8d
Rafa <87162567+Rafasta236@users.noreply.github.com>
2025-05-02 09:26:24 +0100

    w

---
+++ ASLabNode2/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+app.use(express.static(path.join(__dirname, '.'))); // ou 'public' se usares uma pasta pública
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/cliente/12345', (req, res) => {
+    res.json({
+      clienteId: "12345",
+      nome: "João Silva",
+      endereco: {
+        rua: "Rua Exemplo",
+        numero: "42",
+        cidade: "Lisboa",
+        codigoPostal: "1234-567"
+      },
+      consumo: [{
+        mes: "Janeiro",
+        ano: 2023,
+        kWhConsumido: 250,
+        custoTotal: 35.50,
+        dataLeitura: "2023-01-31"
+      }],
+      informacoesAdicionais: {
+        tipoTarifa: "Residencial",
+        fornecedorEnergia: "Empresa XYZ",
+        contratoAtivo: true
+      }
+    });
+});
+
+
+app.listen(port, () => {
+  console.log(`ASLabNode web running on ${port}`)
+})
+
