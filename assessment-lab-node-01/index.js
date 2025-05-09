01b9e1b698154b92272b18a802c1b6ccfe11a036
Tomas-Simoes <tomas20simoes@gmail.com>
2025-05-09 09:46:27 +0100

    new labs

--- assessment-lab-node-01/index.js
+++ assessment-lab-node-01/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express()
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 let minhas_notas = [20, 10, 15, 17];
---
+++ assessment-lab-node-04/public/public/index.js
@@ -0,0 +1,24 @@
+document.getElementById('carregarDados').addEventListener('click', () => {
+    fetch('/cliente/12345')
+      .then(res => res.json())
+      .then(data => {
+        document.getElementById('clienteId').value = data.clienteId;
+        document.getElementById('nome').value = data.nome;
+        document.getElementById('rua').value = data.endereco.rua;
+        document.getElementById('numero').value = data.endereco.numero;
+        document.getElementById('cidade').value = data.endereco.cidade;
+        document.getElementById('codigoPostal').value = data.endereco.codigoPostal;
+  
+        const consumo = data.consumo[0];
+        document.getElementById('mes').value = consumo.mes;
+        document.getElementById('ano').value = consumo.ano;
+        document.getElementById('kWhConsumido').value = consumo.kWhConsumido;
+        document.getElementById('custoTotal').value = consumo.custoTotal;
+        document.getElementById('dataLeitura').value = consumo.dataLeitura;
+  
+        document.getElementById('tipoTarifa').value = data.informacoesAdicionais.tipoTarifa;
+        document.getElementById('fornecedorEnergia').value = data.informacoesAdicionais.fornecedorEnergia;
+        document.getElementById('contratoAtivo').value = data.informacoesAdicionais.contratoAtivo;
+      })
+      .catch(err => console.error('Erro ao buscar dados:', err));
+  });
